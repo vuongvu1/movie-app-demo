4f9a59acb780f7f7b2c7a0c88553d6472e4c89d3
vuongvu <hoangvuong.v99@gmail.com>
2020-02-14 18:27:19 +0700

    Refactor fetch apis

--- src/apis.js
+++ src/apis.js
@@ -3,38 +3,31 @@ import {API_KEY} from 'react-native-dotenv';
 export const BASE_API = 'https://api.themoviedb.org/3';
 export const IMAGE_API = 'https://image.tmdb.org/t/p/';
 
-export const fetchTopRated = () =>
-  fetch(`${BASE_API}/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`)
+const baseFetch = url =>
+  fetch(url)
     .then(response => {
       return response.json();
     })
     .catch(error => {
       console.log(error);
     });
+
+export const fetchTopRated = () =>
+  baseFetch(
+    `${BASE_API}/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+  );
 
 export const fetchTopPopular = () =>
-  fetch(`${BASE_API}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
-    .then(response => {
-      return response.json();
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  baseFetch(
+    `${BASE_API}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
+  );
 
 export const fetchUpcoming = () =>
-  fetch(`${BASE_API}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`)
-    .then(response => {
-      return response.json();
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  baseFetch(
+    `${BASE_API}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
+  );
 
 export const searchMovie = query =>
-  fetch(`${BASE_API}/search/movie?api_key=${API_KEY}&query=${query}&page=1`)
-    .then(response => {
-      return response.json();
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  baseFetch(
+    `${BASE_API}/search/movie?api_key=${API_KEY}&query=${query}&page=1`,
+  );
