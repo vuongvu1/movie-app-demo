ddc6417903a239ea2c2bf335c0050bb5e31ce6e8
vuongvu <hoangvuong.v99@gmail.com>
2020-02-14 22:45:47 +0700

    Add icons on tab bar

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-ionicons';
 
 import LoginScreen from './src/screens/Login';
 import HomeScreen from './src/screens/Home';
@@ -29,7 +30,21 @@ const SearchStackScreen = () => (
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            if (route.name === 'Home') {
+              return <Ionicons name="home" />;
+            } else if (route.name === 'Search') {
+              return <Ionicons name="search" />;
+            }
+            return <Ionicons name="logo-facebook" />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}>
         <Tab.Screen name="Login" component={LoginScreen} />
         <Tab.Screen name="Home" component={HomeStackScreen} />
         <Tab.Screen name="Search" component={SearchStackScreen} />
