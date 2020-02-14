3f6f13ffd75312c5ee8ef5ee4e29aad07fc748e2
vuongvu <hoangvuong.v99@gmail.com>
2020-02-14 18:05:44 +0700

    Add MovieItem component

---
+++ src/components/MovieItem.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {View, Image, Text, StyleSheet} from 'react-native';
+
+import {imageSizes} from '../constants';
+import {getImageUrl} from '../utils';
+
+const MovieItem = ({data: {title, poster_path, release_date, overview}}) => (
+  <View style={styles.container}>
+    <Image
+      style={styles.logo}
+      source={{
+        uri: getImageUrl({
+          fileName: poster_path,
+          size: imageSizes.SEARCH,
+        }),
+      }}
+    />
+    <View style={styles.detail}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.description} numberOfLines={4}>
+        {overview}
+      </Text>
+      <Text style={styles.publishDate}>Published: {release_date}</Text>
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    height: 150,
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+  },
+  logo: {
+    marginHorizontal: 5,
+    width: 100,
+  },
+  detail: {
+    flex: 1,
+    marginHorizontal: 5,
+    justifyContent: 'space-between',
+    padding: 0,
+  },
+  title: {
+    fontSize: 18,
+  },
+  description: {
+    fontSize: 14,
+  },
+  publishDate: {
+    fontSize: 12,
+    color: 'grey',
+  },
+});
+
+export default MovieItem;
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const imageSizes = {
   LOGO: 'w92',
+  SEARCH: 'w154',
   DETAIL: 'w300',
 };
